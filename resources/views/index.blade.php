6255c30a4deb41b8713a670467ffc54fa9a1a8cb
Harry <h7719201@gmail.com>
2022-02-01 16:26:26 +0800

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title')
+安安
+@endsection
+
+@section('main')
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>應用程式名稱 - @yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/animals.css') }}">
+    </head>
+    <body>
+        @yield('main')
+    </body>
+</html>
