81a82bd2e242065a8db334e25e80d92ba1c4c216
Harry <h7719201@gmail.com>
2022-03-14 15:05:27 +0800

    最新消息 - 新增

--- public/js/animal.js
+++ public/js/animal.js
@@ -1,16 +1,48 @@
-$("input[name='save'], #type_add").click(function() {
+function salert(msg, result, timer) {
     new swal({
-        title: "新增成功",
-        icon: "success",
-        timer: 1500
+        title : msg,
+        icon : result,
+        timer : timer
+    });
+}
+
+function ajaxSetup() {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+        }
+    });
+}
+
+$('#newsAdd').click(function(e) {
+    e.preventDefault();
+    ajaxSetup();
+
+    let url = '/news_add/store';
+    let news_title = $('#news_title').val();
+    let news_content = $('#news_content').val();
+
+    $.ajax({
+        url: url,
+        method: 'post',
+        data: {
+            news_title:news_title, 
+            news_content:news_content
+        },
+        success: function() {
+            salert("新增成功", "success", 1500);
+            setTimeout(function() {
+                location.href = "/news_list"
+            }, 1500);
+        }
     });
 });
 
+//產品圖片上傳同步顯示於img中-start
 $('input[name="product_image"]').change(function(e) {
     readURL(e.target);
 });
 
-//產品圖片上傳同步顯示於img中
 function readURL(input){
     if(input.files && input.files[0]){
         var reader = new FileReader();
@@ -20,3 +52,4 @@ function readURL(input){
         reader.readAsDataURL(input.files[0]);
     }
 }
+//產品圖片上傳同步顯示於img中-end
--- resources/views/news/news_add.blade.php
+++ resources/views/news/news_add.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.main')
 @section('news_add')
     <div class="container"><div class="row"><a href="/news_list" class="col-1 btn btn-success">回上一頁</a></div></div><br>
-    <form action="{{ $action }}" method="post">
-    @csrf
+    <form>
     <div class="container">
         <div class="row">
             <div class="col-md-2 col-sm-3 col-xs-3 bg-info bgblue">標題</div>
             <div class="col-md-4 col-sm-9 col-xs-9">
-                <input class="form-control" style="border-redius:5px;" type="text" name="news_title" value="{{ $news_title ?? '' }}">
+                <input class="form-control" style="border-redius:5px;" type="text" id="news_title" value="{{ $news_title ?? '' }}">
             </div>
         </div><br>
         <div class="row">
             <div class="col-md-2 col-sm-3 col-xs-3 bg-info bgblue">內容</div>
             <div class="col-md-4 col-sm-9 col-xs-9">
-                <input class="form-control" style="border-redius:5px;" type="text" name="news_content" value="{{ $news_content ?? '' }}">
+                <input class="form-control" style="border-redius:5px;" type="text" id="news_content" value="{{ $news_content ?? '' }}">
             </div>
         </div><br>
-        <input type="hidden" name="news_id" value="{{ $news_id ?? '' }}">
+        <input type="hidden" id="news_id" value="{{ $news_id ?? '' }}">
         <div class="row">
-            <div class="col-2"><input name="save" class="btn btn-primary" type="submit" value="儲存"></div>
+            <div class="col-2"><button id="newsAdd" class="btn btn-primary">儲存</button></div>
         </div>
     </div>
     </form>
